7a6bfa4ca0dbffebdaa071d507db8dfdf0e460fa
Zach Cyrus <zscyrus31@gmail.com>
2021-08-31 12:13:44 -0400

    Implemented flask rest for swagger ui

--- api/application.py
+++ api/application.py
@@ -1,102 +1,114 @@
 from . import helper
 from flask import Flask, request, Response
+from flask_restx import Api, Resource
+
 import json
 
-app = Flask(__name__)
+flask_app = Flask(__name__)
+app = Api(flask_app)
+
+item_name_space = app.namespace('items', description='Route endpoints for items')
+
 
 @app.route('/')
-def index_page():
-    return "Welcome to Zach's todo list api homepage!"
+class Index_Page(Resource):
+    def get(self):
+        return "Welcome to Zach's todo list api homepage!"
 
-@app.route('/item/new', methods=['POST'])
-def add_item():
-    # Get item from the POST body
-    req_data = request.get_json()
-    item = req_data['item']
+@item_name_space.route('/new', methods=['POST'])
+class New_Item(Resource):
+    def post(self):
+        # Get item from the POST body
+        req_data = request.get_json()
+        item = req_data['item']
 
-    # Add item to the list
-    res_data = helper.add_to_list(item)
+        # Add item to the list
+        res_data = helper.add_to_list(item)
 
-    # Return error if item not added
-    if res_data is None:
-        response = Response("{'error': 'Item not added - " + item + "'}", status=400 , mimetype='application/json')
+        # Return error if item not added
+        if res_data is None:
+            response = Response("{'error': 'Item not added - " + item + "'}", status=400 , mimetype='application/json')
+            return response
+
+        # Return response
+        response = Response(json.dumps(res_data), mimetype='application/json')
+
         return response
 
-    # Return response
-    response = Response(json.dumps(res_data), mimetype='application/json')
+@item_name_space.route('/all')
+class All_Items(Resource):
+    def get(self):
 
-    return response
+        res_data = helper.get_all_items()
 
-@app.route('/items/all')
-def get_all_items():
-
-    res_data = helper.get_all_items()
-
-    # Return response
-    response = Response(json.dumps(res_data), mimetype='application/json')
-    return {
-        "data":res_data
-    },200
+        # Return response
+        response = Response(json.dumps(res_data), mimetype='application/json')
+        return {
+            "data":res_data
+        },200
 
 
-@app.route('/item/status', methods=['GET'])
-def get_item():
+@item_name_space.route('/status', methods=['GET'])
+class Item_Status(Resource):
+    def get(self):
 
-    # Get parameter from the URL ie ?name=
-    item_name = request.args.get('name')
+        # Get parameter from the URL ie ?name=
+        item_name = request.args.get('name')
 
-    # Get items from the helper
-    status = helper.get_item(item_name)
+        # Get items from the helper
+        status = helper.get_item(item_name)
 
-    # Return 404 if item not found
-    if status is None:
-        response = Response("{'error': 'Item Not Found - %s'}"  % item_name, status=404 , mimetype='application/json')
+        # Return 404 if item not found
+        if status is None:
+            response = Response("{'error': 'Item Not Found - %s'}"  % item_name, status=404 , mimetype='application/json')
+            return response
+
+        # Return status
+        res_data = {
+            'status': status
+        }
+
+        response = Response(json.dumps(res_data), status=200, mimetype='application/json')
         return response
 
-    # Return status
-    res_data = {
-        'status': status
-    }
+@item_name_space.route('/update', methods=['PUT'])
+class Update_Item(Resource):
+    def update_status(self):
+        # Get item from the POST body
+        req_data = request.get_json()
+        item = req_data['item']
+        status = req_data['status']
 
-    response = Response(json.dumps(res_data), status=200, mimetype='application/json')
-    return response
+        # Update item in the list
+        res_data = helper.update_status(item, status)
 
-@app.route('/item/update', methods=['PUT'])
-def update_status():
-    # Get item from the POST body
-    req_data = request.get_json()
-    item = req_data['item']
-    status = req_data['status']
+        # Return error if the status could not be updated
+        if res_data is None:
+            response = Response("{'error': 'Error updating item - '" + item + ", " + status   +  "}", status=400 , mimetype='application/json')
+            return response
 
-    # Update item in the list
-    res_data = helper.update_status(item, status)
+        # Return response
+        response = Response(json.dumps(res_data), mimetype='application/json')
 
-    # Return error if the status could not be updated
-    if res_data is None:
-        response = Response("{'error': 'Error updating item - '" + item + ", " + status   +  "}", status=400 , mimetype='application/json')
         return response
 
-    # Return response
-    response = Response(json.dumps(res_data), mimetype='application/json')
 
-    return response
+@item_name_space.route('/remove', methods=['DELETE'])
+class Remove_Item(Resource):
+    def delete(self):
+        # Get item from the POST body
+        req_data = request.get_json()
+        item = req_data['item']
 
+        # Delete item from the list
+        res_data = helper.delete_item(item)
 
-@app.route('/item/remove', methods=['DELETE'])
-def delete_item():
-    # Get item from the POST body
-    req_data = request.get_json()
-    item = req_data['item']
+        # Return error if the item could not be deleted
+        if res_data is None:
+            response = Response("{'error': 'Error deleting item - '" + item +  "}", status=400 , mimetype='application/json')
+            return response
 
-    # Delete item from the list
-    res_data = helper.delete_item(item)
+        # Return response
+        response = Response(json.dumps(res_data), mimetype='application/json')
 
-    # Return error if the item could not be deleted
-    if res_data is None:
-        response = Response("{'error': 'Error deleting item - '" + item +  "}", status=400 , mimetype='application/json')
         return response
-
-    # Return response
-    response = Response(json.dumps(res_data), mimetype='application/json')
-
-    return response
